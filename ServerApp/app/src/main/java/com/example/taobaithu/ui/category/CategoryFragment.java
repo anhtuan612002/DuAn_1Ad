f4379979e02d88bcd91aa35ee3bf51397827a6e8
MrTuanDev <94088646+MrTuanDev@users.noreply.github.com>
2021-11-25 10:06:06 +0700

    Update Category complete

--- ServerApp/app/src/main/java/com/example/taobaithu/ui/category/CategoryFragment.java
+++ ServerApp/app/src/main/java/com/example/taobaithu/ui/category/CategoryFragment.java
@@ -1,24 +1,52 @@
 package com.example.taobaithu.ui.category;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.taobaithu.R;
 import com.example.taobaithu.adater.MyCategoriesAdapter;
 import com.example.taobaithu.common.Common;
+import com.example.taobaithu.common.MySwipeHelper;
 import com.example.taobaithu.common.SpacesItemDecoration;
+import com.example.taobaithu.moldel.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +55,7 @@ import dmax.dialog.SpotsDialog;
 
 public class CategoryFragment extends Fragment {
 
+    private static final int PICK_IMAGE_REQUEST = 1234;
     private CategoryViewModel categoryViewModel;
     Unbinder unbinder;
     @BindView(R.id.recycler_menu)
@@ -34,6 +63,12 @@ public class CategoryFragment extends Fragment {
     AlertDialog dialog;
     LayoutAnimationController layoutAnimationController;
     MyCategoriesAdapter adapter;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    ImageView img_category;
+    private Uri imageUri=null;
+    List<CategoryModel> categoryModels;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -48,8 +83,9 @@ public class CategoryFragment extends Fragment {
             Toast.makeText(getContext(),""+s,Toast.LENGTH_SHORT).show();
             dialog.dismiss();
         });
-        categoryViewModel.getCategoryListMultable().observe(getViewLifecycleOwner(),categoryModels -> {
+        categoryViewModel.getCategoryListMultable().observe(getViewLifecycleOwner(),categoryModelsList -> {
             dialog.dismiss();
+            categoryModels = categoryModelsList;
             adapter = new MyCategoriesAdapter(getContext(),categoryModels);
             recycler_menu.setAdapter(adapter);
             recycler_menu.setLayoutAnimation(layoutAnimationController);
@@ -57,25 +93,110 @@ public class CategoryFragment extends Fragment {
         return root;
     }
     private void initViews() {
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
         dialog = new SpotsDialog.Builder().setContext(getContext()).setCancelable(false).build();
         dialog.show();
         layoutAnimationController = AnimationUtils.loadLayoutAnimation(getContext(),R.anim.layout_item_from_left);
-        GridLayoutManager layoutManager = new GridLayoutManager(getContext(),2);
-        layoutManager.setOrientation(RecyclerView.VERTICAL);
-        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-            @Override
-            public int getSpanSize(int position) {
-                if(adapter != null){
-                    switch (adapter.getItemViewType(position)){
-                        case Common.DEFAULT_COLUMN_COUNT: return 1;
-                        case Common.FULL_WIDTH_COLUMN:return 2;
-                        default:return -1;
-                    }
-                }
-                return -1;
-            }
-        });
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+
         recycler_menu.setLayoutManager(layoutManager);
-        recycler_menu.addItemDecoration(new SpacesItemDecoration(8));
+        recycler_menu.addItemDecoration(new DividerItemDecoration(getContext(),layoutManager.getOrientation()));
+        MySwipeHelper mySwipeHelper = new MySwipeHelper(getContext(),recycler_menu ,200) {
+            @Override
+            public void instantiateMyButton(RecyclerView.ViewHolder viewHolder, List<MyButton> buf) {
+                    buf.add(new MyButton(getContext(),"Update",30,0, Color.parseColor("#560027"),
+                            pos -> {
+                          Common.categorySelected = categoryModels.get(pos);
+                        showUpdateDialog();
+                            }
+                    ));
+            }
+        };
+    }
+
+    private void showUpdateDialog() {
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(getContext());
+        builder.setTitle("Update");
+        builder.setMessage("Please fill information");
+
+        View itemView = LayoutInflater.from(getContext()).inflate(R.layout.layout_update_category,null);
+        EditText edt_category_name = (EditText) itemView.findViewById(R.id.edt_category_name);
+        img_category = (ImageView) itemView.findViewById(R.id.img_category);
+//        set Data
+        edt_category_name.setText(new StringBuilder("").append(Common.categorySelected.getName()));
+        Glide.with(getContext()).load(Common.categorySelected.getImage()).into(img_category);
+//        Set Event
+        img_category.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent,"Select Picture"),PICK_IMAGE_REQUEST);
+        });
+
+        builder.setNegativeButton("CANCEL", (dialogInterface, i) -> dialogInterface.dismiss());
+        builder.setPositiveButton("UPDATE", (dialogInterface, i) -> {
+
+            Map<String,Object> updateData = new HashMap<>();
+            updateData.put("name",edt_category_name.getText().toString());
+            if(imageUri != null)
+            {
+                /** sài firebase up ảnh*/
+                dialog.setMessage("UpLoading...");
+                dialog.show();
+                String unique_name = UUID.randomUUID().toString();
+                StorageReference imageFolder = storageReference.child("images/"+unique_name);
+
+                imageFolder.putFile(imageUri)
+                        .addOnFailureListener(e -> {
+                            dialog.dismiss();
+                            Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }).addOnCompleteListener(task -> {
+                                    dialog.dismiss();
+                                    imageFolder.getDownloadUrl().addOnSuccessListener(uri ->
+                                    {
+                                        updateData.put("image",uri.toString());
+                                        updateCategory(updateData);
+                                    });
+                        }).addOnProgressListener(snapshot -> {
+                                double progess = (100.0 * snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
+                                dialog.setMessage(new StringBuilder("Uploading: ").append(progess).append("%"));
+                        });
+            }
+            else
+            {
+                updateCategory(updateData);
+            }
+
+        });
+        builder.setView(itemView);
+        androidx.appcompat.app.AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void updateCategory(Map<String, Object> updateData) {
+        FirebaseDatabase.getInstance()
+                .getReference(Common.CATEGORY_REF)
+                .child(Common.categorySelected.getMenu_id())
+                .updateChildren(updateData)
+                .addOnFailureListener(e -> Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show())
+                .addOnCompleteListener(task -> {
+                    categoryViewModel.loadCategories();
+                    Toast.makeText(getContext(), "Update success", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST&&resultCode== Activity.RESULT_OK)
+        {
+            if(data != null&&data.getData() !=null){
+                imageUri = data.getData();
+                img_category.setImageURI(imageUri);
+            }
+        }
     }
 }
--- ServerApp/app/src/main/java/com/example/taobaithu/ui/category/CategoryViewModel.java
+++ ServerApp/app/src/main/java/com/example/taobaithu/ui/category/CategoryViewModel.java
@@ -36,7 +36,7 @@ public class CategoryViewModel extends ViewModel implements ICategoryCallbackLis
         return categoryListMultable;
     }
 
-    private void loadCategories() {
+    public void loadCategories() {
         List<CategoryModel> tempList = new ArrayList<>();
         DatabaseReference categoryRef = FirebaseDatabase.getInstance().getReference(Common.CATEGORY_REF);
         categoryRef.addListenerForSingleValueEvent(new ValueEventListener() {
